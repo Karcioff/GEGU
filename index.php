58553a1d4a8a1a4a7dd09bbd974f34e637ab80cd
Rachele <Rachele@192.168.1.117>
2017-09-27 22:32:01 +0200

    Index made with PHP

--- index.php
+++ index.php
@@ -1,15 +1,45 @@
+
+<!DOCTYPE html>
 <?php
 session_start();
+require_once 'connection/connect.php';
 $logged = false;
 if (isset($_SESSION['logged']) && $_SESSION['logged'] == TRUE && isset($_SESSION['log_username']) && $_SESSION['log_username'] != "") {
     $logged = true;
-};
+}
+?>
+<html lang="it">
 
-//FUNZIONI INDEX PAGE
-function navbar($logged) {
-    if ($logged) {
-        //utente LOGGATO
-        echo '        <nav class="light-blue lighten-1 z-depth-1" role="navigation">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
+        <title>index</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <style>
+            body {
+                display: flex;
+                flex-direction: column;
+                font-family: 'Open Sans', sans-serif;
+                background-color: rgb(239, 247, 252);
+            }
+
+            main {
+                flex: 1 0 auto;
+            }
+
+            .parallax-container {
+                height: 600px;
+                background-repeat: no-repeat;
+                background-size: contain;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+        if ($logged) {
+            echo '<nav class="light-blue lighten-1 z-depth-1" role="navigation">
             <a href="#" data-activates="slide-out" class="button-collapse show-on-large"><i class="material-icons">menu</i></a>
             <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo side">GE.GU ' . $_SESSION["log_username"] . '</a>
                 <ul class="right hide-on-med-and-down">
@@ -41,10 +71,10 @@ function navbar($logged) {
                 <li><a href="insertart.php" class="waves-effect"><i class="material-icons">comment</i>Aggiungi articolo</a></li>
             </ul>
         </nav>';
-    }
-    // utente OSPITE
-    else {
-        echo '        <nav class="light-blue lighten-1 z-depth-1" role="navigation">
+        }
+        // utente OSPITE
+        else {
+            echo '        <nav class="light-blue lighten-1 z-depth-1" role="navigation">
             <a href="#" data-activates="slide-out" class="button-collapse show-on-large"><i class="material-icons">menu</i></a>
             <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo side">GE.GU utente ospite</a>
                 <ul class="right hide-on-med-and-down">
@@ -75,126 +105,59 @@ function navbar($logged) {
                 <li><a href="#!" class="waves-effect"><i class="material-icons">terrain</i>Campi estivi</a></li>
             </ul>
         </nav>';
-    }
-}
+        }
+        ?>
 
-//INIZIO PAGINA
-echo '<!DOCTYPE html>
-<html lang="it">
+        <!--Inserimento articoli -->
+        <div class="container">
+            <?php
+            if ($logged) {
+                $res = '';
+                $result = $conn->query("SELECT * FROM articoli");
+                while ($row = $result->fetch_assoc()) {
+                    $aut = $row["ART_AUTORE"];
+                    $tit = $row["ART_TITOLO"];
+                    $art = $row["ART_TESTO"];
+                    echo <<<HTML
+                '<div class = "col s12 m12">
+                        <div class = "card white z-depth-1">
+                            <div class = "card-content blue-text">
+                                <span class = "card-title black-text"><b>$tit</b></span>
+                                <p>$art</p>
+                                <small>By : $aut</small>
+                            </div>
+                            <div class = "card-action grey lighten-5">
+                                <a href = "#" class = "blue-text">Accetta</a>
+                                <a href = "#" class = "blue-text">Rifiuta</a>
+                            </div>
+                        </div>
+                </div >
+HTML;
+                }
+            }
+            ?>
+        </div>
 
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
-        <title>index</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-
-    </head>
-    <body>';
-
-navbar($logged);
-
-echo '<footer class="page-footer blue">
-    <div class="container row">
-        Software developed by: HEROES SOFTWARE ©2017
-    </div>
-</footer>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-<script>$(".button-collapse").sideNav();</script>';
-
-if ($logged) {
-    echo '<script> $().ready(function () {
+        <footer class="page-footer blue">
+            <div class="container row">
+                Software developed by: HEROES SOFTWARE ©2017
+            </div>
+        </footer>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+        <script>$(".button-collapse").sideNav();</script>
+        <?php
+        if ($logged) {
+            echo '<script> $().ready(function () {
                 var username = "' . $_SESSION['log_username'] . '";               
                     Materialize.toast(username, 4000);                
             }); </script>';
-};
+        }
 
-if (isset($_SESSION['art_inserted']) && $_SESSION['art_inserted'] == true) {
-    echo ' <script> $().ready(function () {              
+        if (isset($_SESSION['art_inserted']) && $_SESSION['art_inserted'] == true) {
+            echo ' <script> $().ready(function () {              
                     Materialize.toast( "Articolo pubblicato con successo", 2000);                
  }); </script>';
-    unset($_SESSION['art_inserted']);
-};
-echo '</body>
-</html>';
-exit();
+            unset($_SESSION['art_inserted']);
+        }
 ?>
-
-<!--<div class="container">
-    <div class="col s12 m12">
-        <div class="card white z-depth-1">
-            <div class="card-content blue-text">
-                <span class="card-title black-text"><b>Richiesta di riunione</b></span>
-                <p>I capi richiedono una riunione per trovare una maniera per entrare in una scarpa</p>
-            </div>
-            <div class="card-action grey lighten-5">
-                <a href="#" class="blue-text">Accetta</a>
-                <a href="#" class="blue-text">Rifiuta</a>
-            </div>
-        </div>
-    </div>
-
-     
-    <div class="col s12 m12">
-        <div class="card white z-depth-1">
-            <div class="card-content blue-text">
-                <span class="card-title black-text"><b>Appuntamento con: <u>Don Lio</u></b></span>
-                <p>Mandriano, ti devi confessare. Ora.</p>
-            </div>
-            <div class="card-action grey lighten-5">
-                <a href="#" class="blue-text">Accetta</a>
-                <a href="#" class="blue-text">Rifiuta</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="col s12 m12">
-        <div class="card white z-depth-1">
-            <div class="card-content blue-text">
-                <span class="card-title black-text"><b>O.D.G. Co.Ca</b></span><br>
-                <ul class="collection">
-                    <li class="collection-item">Fuochi d'autunno</li>
-                    <li class="collection-item">Verifica Staff</li>
-                    <li class="collection-item">Nuove Staff</li>
-                    <li class="collection-item">Guerra degli Akela</li>
-                </ul>
-
-            </div>
-            <div class="card-action grey lighten-5">
-                <a href="#" class="blue-text">Accetta</a>
-                <a href="#" class="blue-text">Rifiuta</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="col s12 m12">
-        <div class="card white z-depth-1">
-            <div class="card-content blue-text">
-                <span class="card-title black-text"><b>O.D.G. Co.Ca mese successivo</b></span>
-                <p>Fiesta.</p>
-            </div>
-            <div class="card-action grey lighten-5">
-                <a href="#" class="blue-text">Accetta</a>
-                <a href="#" class="blue-text">Rifiuta</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="col s12 m12">
-        <div class="card white z-depth-1">
-            <div class="card-content blue-text">
-                <span class="card-title black-text"><b>Guidoncini verdi</b></span>
-                <p>Richiesta presenza.</p>
-            </div>
-            <div class="card-action grey lighten-5">
-                <a href="#" class="blue-text">Accetta</a>
-                <a href="#" class="blue-text">Rifiuta</a>
-            </div>
-        </div>
-    </div>
-</div>-->
-
-
-
